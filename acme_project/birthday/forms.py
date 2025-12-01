9deb887986414dd43dbeec069720840b2f8c6070
Данила Горохов <quickemoji@gmail.com>
2025-12-01 07:45:45 +0400

    123

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,38 @@
+# birthday/forms.py
+from django import forms
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
+
+from .models import Birthday
+
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
+
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        } 
+
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+    
+
+    def clean(self):
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            ) 
